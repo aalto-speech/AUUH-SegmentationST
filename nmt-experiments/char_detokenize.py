9c590fe0c84fc8f89ae41c30d9f773c5106fa0c4
Aku Rouhe <rouheaku@puhti-login1.bullx>
2022-05-03 17:45:11 +0300

    NMT experiment pipeline

--- nmt-experiments/char_detokenize.py
+++ nmt-experiments/char_detokenize.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import pathlib
+import more_itertools
 
 conversions = {"@@": " @@",
                "<w>": " "}
@@ -11,18 +12,39 @@ def run_on_line(line):
     output = "".join(tokens)
     return output
 
-def run_on_file(inputfile):
-    with open(inputfile) as fin, \
-         open(inputfile+".detok", "w") as fout:
-        for line in fin:
-            output = run_on_line(line)
-            print(output, file=fout)
+def get_meta_data(line):
+    line = line.strip()
+    if line.count("\t") == 2:
+        with_classes = True
+    elif line.count("\t") == 1:
+        with_classes = False
+    else:
+        without_tabs = line.replace('\t','TAB')
+        raise ValueError(f"Don't know how to process line: {without_tabs}")
+    if with_classes:
+        source, target, word_class = line.split("\t")
+    else:
+        source, target = line.split("\t")
+        word_class = None
+    return source, target, word_class
+
+def run_detokenization(translationfile, metadatafile, outputfile):
+    with open(translationfile) as translation_fin, \
+         open(metadatafile) as metadata_fin, \
+         open(outputfile, "w") as fout:
+        for translation, metadata in more_itertools.zip_equal(translation_fin, metadata_fin):
+            detokenized = run_on_line(translation)
+            source, target, word_class = get_meta_data(metadata)
+            if word_class is not None:
+                print(source, detokenized, word_class, file=fout, sep="\t")
+            else:
+                print(source, detokenized, file=fout, sep="\t")
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser("Detokenizer for character tokenized data with Sigmorphon 22 data and Marian")
-    parser.add_argument("input", help="Path to input file (e.g. fra.word.dev.decoded), "
-    "will output to input.detok")
+    parser.add_argument("translation", help="Path to translation output file (e.g. fra.word.dev.decoded)")
+    parser.add_argument("metadata", help="Path to file with metadata")
+    parser.add_argument("outputfile", help="Path to file with metadata")
     args = parser.parse_args()
-    run_on_file(args.input)
-
+    run_detokenization(args.translation, args.metadata, args.outputfile)
--- nmt-experiments/train-baseline.sh
+++ nmt-experiments/train-baseline.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=train-nmt
-#SBATCH --account=project_205881
-#SBATCH --time=0:10:0
-#SBATCH --partition=gputest
+#SBATCH --account=project_2005881
+#SBATCH --time=16:00:00
+#SBATCH --partition=gpu
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=16G
-#SBTACH --gres=gpu:v100:1
+#SBATCH --gres=gpu:v100:1
 
 DATAROOT="/scratch/project_2005881/2022SegmentationST/data/"
 PROCESSED="/scratch/project_2005881/2022SegmentationST/nmt-experiments/processed-data/"
@@ -31,24 +31,61 @@ mkdir -p $expdir
 
 source path.sh
 
-python3 char_tokenize.py \
-  ${DATAROOT}/${lang}.${task}.train.tsv \
-  ${PROCESSED}/${lang}.${task}.train
-python3 char_tokenize.py \
-  ${DATAROOT}/${lang}.${task}.dev.tsv \
-  ${PROCESSED}/${lang}.${task}.dev
+### STAGE 1: Preprocess data ###
 
+if [ -d ${PROCESSED}/${lang}.${task} ]; then
+  echo "Using existing preprocessed data in"
+  echo " ${PROCESSED}/${lang}.${task}"
+else
+  echo "Preprocess data to ${PROCESSED}/${lang}.${task}"
+  mkdir ${PROCESSED}/${lang}.${task}
+  python3 char_tokenize.py \
+    ${DATAROOT}/${lang}.${task}.train.tsv \
+    ${PROCESSED}/${lang}.${task}/train
+  python3 char_tokenize.py \
+    ${DATAROOT}/${lang}.${task}.dev.tsv \
+    ${PROCESSED}/${lang}.${task}/dev
+fi
+
+### STAGE 2: Train model ###
 
 marian \
   --model $expdir/model.npz \
   --train-sets \
-    ${PROCESSED}/${ḷang}.${task}.train.src.txt \
-    ${PROCESSED}/${ḷang}.${task}.train.tgt.txt \
+    ${PROCESSED}/${lang}.${task}/train.src.txt \
+    ${PROCESSED}/${lang}.${task}/train.tgt.txt \
   --vocabs \
-    ${PROCESSED}/${ḷang}.${task}.train.src.vocab \
-    ${PROCESSED}/${ḷang}.${task}.train.tgt.vocab \
+    ${PROCESSED}/${lang}.${task}/train.src.vocab \
+    ${PROCESSED}/${lang}.${task}/train.tgt.vocab \
   --valid-sets \
-    ${PROCESSED}/${ḷang}.${task}.dev.src.txt \
-    ${PROCESSED}/${ḷang}.${task}.dev.tgt.txt \
+    ${PROCESSED}/${lang}.${task}/dev.src.txt \
+    ${PROCESSED}/${lang}.${task}/dev.tgt.txt \
   --config ${CONFIGDIR}/${RUN_FAMILY}.yaml
 
+### STAGE 3: Decode dev data ###
+
+marian-decoder \
+  --config ${expdir}/model.npz.best-cross-entropy.npz.decoder.yml \
+  --input ${PROCESSED}/${lang}.${task}/dev.src.txt \
+  --output ${expdir}/decode-dev.txt
+
+### STAGE 4: Detokenize ###
+
+python char_detokenize.py ${expdir}/decode-dev.txt ${DATAROOT}/${lang}.${task}.dev.tsv ${expdir}/decode-dev.tsv
+
+### STAGE 5: Compute the evaluation metrics ###
+
+if [ "$task" == "word" ]; then
+  python /scratch/project_2005881/2022SegmentationST/evaluation/evaluate.py \
+    --category \
+    --gold ${DATAROOT}/${lang}.${task}.dev.tsv \
+    --guess ${expdir}/decode-dev.tsv \
+    > ${expdir}/decode-dev.evaluation
+else
+  python /scratch/project_2005881/2022SegmentationST/evaluation/evaluate.py \
+    --gold ${DATAROOT}/${lang}.${task}.dev.tsv \
+    --guess ${expdir}/decode-dev.tsv \
+    > ${expdir}/decode-dev.evaluation
+fi
+# Here's the output:
+tail -n5 ${expdir}/decode-dev.evaluation
